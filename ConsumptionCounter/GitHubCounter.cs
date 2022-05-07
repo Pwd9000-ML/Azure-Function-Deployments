7a3ea6ff19f60278a24526faad1ea9bcb82eb324
M L <magiconion_M@hotmail.com>
2022-05-07 14:29:34 +0100

    test

--- ConsumptionCounter/GitHubCounter.cs
+++ ConsumptionCounter/GitHubCounter.cs
@@ -27,7 +27,7 @@ namespace GitHubCounter
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully (GitHub).";
 
             return new OkObjectResult(responseMessage);
         }
