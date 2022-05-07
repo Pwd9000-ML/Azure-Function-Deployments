1c8209656236cc20662ba2a679229010109ffec6
M L <magiconion_M@hotmail.com>
2022-05-07 16:29:09 +0100

    test

---
+++ ConsumptionCounter/ConsumptionCounter.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace GitHub.Counter
+{
+    public static class ConsumptionCounter
+    {
+        [FunctionName("ConsumptionCounter")]
+        public static object Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: Environment.GetEnvironmentVariable("DatabaseName"),
+                collectionName: Environment.GetEnvironmentVariable("CollectionName"),
+                ConnectionStringSetting = Environment.GetEnvironmentVariable("CosmosDBConnectionString"),
+                Id = "github_main",
+                PartitionKey = "github_main")] CounterJson counter,
+            ILogger log)
+        {
+            return counter;
+        }
+    }
+
+    public class CounterJson
+    {
+        [JsonPropertyName("id")]
+        public string Id {get;set;}
+
+        [JsonPropertyName("count")]
+        public int Count {get;set;}
+    }
+}
