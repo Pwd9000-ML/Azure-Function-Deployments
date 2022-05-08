327cf6c98c51ea173ec0532fb80a7cba577b049b
M L <magiconion_M@hotmail.com>
2022-05-08 11:54:05 +0100

    go

--- ConsCount01/ConsCount01.cs
+++ ConsCount01/ConsCount01.cs
@@ -1,24 +1,18 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.Azure.Functions.Worker;
-using System.Text.Json.Serialization;
+
 
 namespace My.Function
 {
     public static class ConsCount01
     {
         [FunctionName("ConsCount01")]
-        // [CosmosDBOutput("%DatabaseName%", "%CollectionName%", ConnectionStringSetting = "CosmosConnection")]
+        [CosmosDBOutput("%DatabaseName%", "%CollectionName%", ConnectionStringSetting = "CosmosConnection")]
         public static object Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(
+            [CosmosDBInput(
                 databaseName: "VisitorCounter",
                 collectionName: "GitHubCounter",
                 ConnectionStringSetting = "CosmosConnection",
@@ -26,17 +20,17 @@ namespace My.Function
                 PartitionKey = "github_main")] CounterJson counter01,
             FunctionContext context)
         {
-            //     counter.Count++;
+            counter01.Count++;
             return counter01;
         }
     }
 
     public class CounterJson
     {
-        [JsonPropertyName("id")]
+        [System.Text.Json.Serialization.JsonPropertyName("id")]
         public string Id { get; set; }
 
-        [JsonPropertyName("count")]
+        [System.Text.Json.Serialization.JsonPropertyName("count")]
         public int Count { get; set; }
     }
 }
