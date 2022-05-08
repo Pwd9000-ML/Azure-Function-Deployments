69b2c28e918da15382ea977a4aaef520a04f4333
M L <magiconion_M@hotmail.com>
2022-05-08 10:04:55 +0100

    update

---
+++ ConsumptionCounter/ConsumptionCounter.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Azure.Functions.Worker;
+
+
+namespace GitHub.Counter
+{ 
+    public static class ConsumptionCounter
+    {
+        [FunctionName("ConsumptionCounter")]
+        [CosmosDBOutput("%DatabaseName%", "%CollectionName%", ConnectionStringSetting = "CosmosConnection", CreateIfNotExists = true)]
+        public static object Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDBInput("%DatabaseName%", "%CollectionName%", ConnectionStringSetting = "CosmosConnection", Id = "github_main", PartitionKey = "github_main")] CounterJson counter,
+            ILogger log)
+        {
+            counter.Count++;
+            return counter;
+        }
+    }
+
+    public class CounterJson
+    {
+        [JsonPropertyName("id")]
+        public string Id {get;set;}
+
+        [JsonPropertyName("count")]
+        public int Count {get;set;}
+    }
+}
+
+// dotnet add package Microsoft.Azure.WebJobs.Extensions.CosmosDB
+// dotnet add package Microsoft.Azure.Functions.Worker.Extensions.CosmosDB
